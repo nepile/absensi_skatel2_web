d2da60d7a46f005a75007987bde69c1f5eb85fd3
nepile <nevillejro.laia@gmail.com>
2023-07-30 00:23:29 +0700

    Update data presensi view

--- resources/views/core/presensi.blade.php
+++ resources/views/core/presensi.blade.php
@@ -9,27 +9,15 @@
     <div class="col-12">
         <div class="card" style="background: #fff; border: 1px solid #eee; box-shadow: rgba(0, 0, 0, 0.04) 0px 3px 5px;">
             <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-striped" id="data">
-                        <thead style="background: #af1f22; color: #fff">
-                            <tr>
-                                <th>No.</th>
-                                <th>NIS/NIP</th>
-                                <th>Kelas</th>
-                                <th>Detail</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1.</td>
-                                <td>206765</td>
-                                <td>X RPL 1</td>
-                                <td>
-                                    <button class="btn" style="background: #af1f22"><i class="fa fa-eye text-light"></i></button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                <div style="border-bottom: 1px solid #eee; border-width: 100%;">
+                    <a href="" class="text-decoration-none d-inline-block">
+                        <h6 class="mb-3">Hari Ini <span class="text-dark">({{ date('d M Y', strtotime('now')) }})</span></h6>
+                    </a>
+                </div>
+                <div style="border-bottom: 1px solid #eee; border-width: 100%;">
+                    <a href="" class="text-decoration-none d-inline-block">
+                        <h6 class="my-3">Rekapan Persensi</h6>
+                    </a>
                 </div>
             </div>
         </div>

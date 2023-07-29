102945c42794461f03894828f49fcdccd99aaa68
nepile <nevillejro.laia@gmail.com>
2023-07-30 00:30:24 +0700

    Setup & Configuration JWT lib

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,8 +8,9 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -37,5 +38,15 @@ class User extends Authenticatable
     public function class(): BelongsTo
     {
         return $this->belongsTo(ClassUser::class, 'class_id');
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
     }
 }

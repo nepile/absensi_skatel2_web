d3b73a56e965129ad07fe67ef8d51508ec00b693
nepile <nevillejro.laia@gmail.com>
2023-07-30 05:11:28 +0700

    Setup logic API  update password

---
+++ app/Http/Controllers/Core/UpdatePasswordController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Core;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class UpdatePasswordController extends Controller
+{
+    # change password
+    public function changePassword(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'oldPassword'       => 'required',
+            'newPassword'       => 'required',
+            'retypePassword'    => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success'   => false,
+                'message'   => 'Semua inputan tidak boleh kosong'
+            ], 400);
+        }
+
+        if (!Hash::check($request->oldPassword, JWTAuth::user()->password)) {
+            return response()->json([
+                'success'   => false,
+                'message'   => 'Password anda saat ini salah'
+            ], 400);
+        }
+
+        if ($request->retypePassword == $request->newPassword) {
+            DB::table('users')->where('user_id', $request->user_id)->update([
+                'password'  => Hash::make($request->newPassword)
+            ]);
+
+            return response()->json([
+                'success'   => true,
+                'message'   => 'Password anda berhasil diperbarui'
+            ], 200);
+        } else {
+            return response()->json([
+                'success'   => false,
+                'message'   => 'Konfirmasi password baru anda tidak sama'
+            ], 400);
+        }
+    }
+}

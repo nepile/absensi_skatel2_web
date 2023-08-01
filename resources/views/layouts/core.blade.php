fbec3c48323570ec3a4c87e0a5b98576ecc67d36
nepile <nevillejro.laia@gmail.com>
2023-08-01 21:59:48 +0700

    Add favicon in core layout

--- resources/views/layouts/core.blade.php
+++ resources/views/layouts/core.blade.php
@@ -7,6 +7,7 @@
   <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
+  <link rel="icon" href="{{ asset('img/logo.png') }}">
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/dataTables.bootstrap5.min.css">
 </head>
 <body>
